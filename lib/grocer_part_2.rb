70a7a4fa671fedd140455e980be3e7c95f8a5b6d
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-14 04:46:20 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -9,7 +9,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     coupon.each do |keys, values|
       binding.pry
-      name = keys[:item]
+      name = coupon[:item]
       binding.pry
     end
   end
